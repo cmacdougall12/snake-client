546165142c88919d566707cf91899feea8127e27
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-02 21:22:59 +0000

    added movements and messages to input.js

--- client.js
+++ client.js
@@ -29,8 +29,3 @@ const connect = function () {
 module.exports = connect;
 
 
- // setInterval(function(){conn.write('Move: up')},50);
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,10 +10,43 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
+
   if (key === '\u0003') {
     process.exit();
+  }
+  //up
+  if (key === '\u0077') {
+    connection.write('Move: up');
+  }
+
+  //down
+  if (key === '\u0073') {
+    connection.write('Move: down');
+
+  }
+  //right
+  if (key === '\u0064') {
+    connection.write('Move: right');
+  }
+  //left
+  if (key === '\u0061') {
+    connection.write('Move: left');
+  }
+
+  //messages
+  if (key === '\u0031') {
+    connection.write('Say: Got it!!');
+  }
+   if (key === '\u0032') {
+    connection.write('Say: Hungry!!');
+  }
+
+  if (key === '\u0033') {
+    connection.write('Say: Cya!!');
   }
 };
 
 module.exports = setupInput;
+
+ // setInterval(function(){conn.write('Move: up')},50);
